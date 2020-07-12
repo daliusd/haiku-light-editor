9a861e2b84cb296e23fef1a7c6072b995cabf82a
Dalius Dobravolskas <dalius.dobravolskas@gmail.com>
2020-07-12 19:30:40 +0300

    Simple editor with editable.

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 
-import { ExampleComponent } from 'haiku-light-editor'
+import { Editor, Editable } from 'haiku-light-editor'
 import 'haiku-light-editor/dist/index.css'
 
+const editable: Editable = {
+  width: 20,
+  height: 20
+}
+
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return <Editor width={600} height={400} editable={editable} />
 }
 
 export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,45 @@
 import * as React from 'react'
 import styles from './styles.module.css'
 
-interface Props {
-  text: string
+export interface Editable {
+  width: number
+  height: number
 }
 
-export const ExampleComponent = ({ text }: Props) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+interface Props {
+  width: number
+  height: number
+  editable: Editable
+}
+
+export const Editor = ({ width, height, editable }: Props) => {
+  // TODO: 10px margin default now
+  const ppmm = Math.min(
+    (width - 20) / editable.width,
+    (height - 20) / editable.height
+  )
+
+  const editableWidth = editable.width * ppmm
+  const editableHeight = editable.height * ppmm
+
+  const x = (width - editableWidth) / 2
+  const y = (height - editableHeight) / 2
+
+  return (
+    <div
+      style={{ width: `${width}px`, height: `${height}px` }}
+      className={styles.editor}
+    >
+      <div
+        style={{
+          width: editableWidth,
+          height: editableHeight,
+          left: x,
+          top: y,
+          position: 'absolute',
+          background: '#ffffff'
+        }}
+      />
+    </div>
+  )
 }
