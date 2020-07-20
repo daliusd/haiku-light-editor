08eded9630adfd36649fc18d2b822d15a05df8cd
Dalius Dobravolskas <dalius.dobravolskas@gmail.com>
2020-07-20 19:44:26 +0300

    Exif tests added.

--- example/e2e/visual.test.js
+++ example/e2e/visual.test.js
@@ -1,24 +1,24 @@
-const fs = require('fs')
-const pixelmatch = require('pixelmatch')
-const PNG = require('pngjs').PNG
+const fs = require('fs');
+const pixelmatch = require('pixelmatch');
+const PNG = require('pngjs').PNG;
 
-const testDir = './e2e/screenshots'
-const goldenDir = './e2e/golden'
+const testDir = './e2e/screenshots';
+const goldenDir = './e2e/golden';
 
 function compareScreenshots(fileName) {
-  let testFileName = `${testDir}/${fileName}.png`
-  let goldenFileName = `${goldenDir}/${fileName}.png`
-  const img1 = PNG.sync.read(fs.readFileSync(testFileName))
+  let testFileName = `${testDir}/${fileName}.png`;
+  let goldenFileName = `${goldenDir}/${fileName}.png`;
+  const img1 = PNG.sync.read(fs.readFileSync(testFileName));
   if (!fs.existsSync(goldenFileName)) {
-    fs.copyFileSync(testFileName, goldenFileName)
-    return 0
+    fs.copyFileSync(testFileName, goldenFileName);
+    return 0;
   }
-  const img2 = PNG.sync.read(fs.readFileSync(goldenFileName))
+  const img2 = PNG.sync.read(fs.readFileSync(goldenFileName));
 
-  expect(img1.width).toEqual(img2.width)
-  expect(img1.height).toEqual(img2.height)
+  expect(img1.width).toEqual(img2.width);
+  expect(img1.height).toEqual(img2.height);
 
-  const diff = new PNG({ width: img1.width, height: img2.height })
+  const diff = new PNG({ width: img1.width, height: img2.height });
   const numDiffPixels = pixelmatch(
     img1.data,
     img2.data,
@@ -26,59 +26,65 @@ function compareScreenshots(fileName) {
     img1.width,
     img1.height,
     { threshold: 0.05 }
-  )
+  );
 
-  expect(numDiffPixels).toEqual(0)
+  expect(numDiffPixels).toEqual(0);
 }
 
 async function takeAndCompareScreenshot(route, filePrefix) {
-  let fileName = filePrefix + '/' + (route ? route : 'index')
+  let fileName = filePrefix + '/' + (route ? route : 'index');
 
-  await page.goto(`http://127.0.0.1:4000/${route}`)
-  await page.screenshot({ path: `${testDir}/${fileName}.png` })
+  await page.goto(`http://127.0.0.1:4000/${route}`);
+  await page.screenshot({ path: `${testDir}/${fileName}.png` });
 
-  return compareScreenshots(fileName)
+  return compareScreenshots(fileName);
 }
 
 describe('👀 screenshots are correct', function () {
   beforeAll(async function () {
-    if (!fs.existsSync(testDir)) fs.mkdirSync(testDir)
-    if (!fs.existsSync(goldenDir)) fs.mkdirSync(goldenDir)
+    if (!fs.existsSync(testDir)) fs.mkdirSync(testDir);
+    if (!fs.existsSync(goldenDir)) fs.mkdirSync(goldenDir);
 
-    if (!fs.existsSync(`${testDir}/wide`)) fs.mkdirSync(`${testDir}/wide`)
-    if (!fs.existsSync(`${testDir}/narrow`)) fs.mkdirSync(`${testDir}/narrow`)
-    if (!fs.existsSync(`${goldenDir}/wide`)) fs.mkdirSync(`${goldenDir}/wide`)
+    if (!fs.existsSync(`${testDir}/wide`)) fs.mkdirSync(`${testDir}/wide`);
+    if (!fs.existsSync(`${testDir}/narrow`)) fs.mkdirSync(`${testDir}/narrow`);
+    if (!fs.existsSync(`${goldenDir}/wide`)) fs.mkdirSync(`${goldenDir}/wide`);
     if (!fs.existsSync(`${goldenDir}/narrow`))
-      fs.mkdirSync(`${goldenDir}/narrow`)
-  })
+      fs.mkdirSync(`${goldenDir}/narrow`);
+  });
 
   describe('wide screen', function () {
     beforeEach(async function () {
-      return page.setViewport({ width: 800, height: 600 })
-    })
+      return page.setViewport({ width: 800, height: 600 });
+    });
     it('/simple', async function () {
-      return await takeAndCompareScreenshot('simple', 'wide')
-    })
+      return await takeAndCompareScreenshot('simple', 'wide');
+    });
     it('/black', async function () {
-      return await takeAndCompareScreenshot('black', 'wide')
-    })
+      return await takeAndCompareScreenshot('black', 'wide');
+    });
     it('/image', async function () {
-      return await takeAndCompareScreenshot('image', 'wide')
-    })
-  })
+      return await takeAndCompareScreenshot('image', 'wide');
+    });
+    it('/exif', async function () {
+      for (let i = 0; i < 9; i++) {
+        await takeAndCompareScreenshot(`exifp${i}`, 'wide');
+        await takeAndCompareScreenshot(`exifl${i}`, 'wide');
+      }
+    });
+  });
 
   describe('narrow screen', function () {
     beforeEach(async function () {
-      return page.setViewport({ width: 375, height: 667 })
-    })
+      return page.setViewport({ width: 375, height: 667 });
+    });
     it('/simple', async function () {
-      return await takeAndCompareScreenshot('simple', 'narrow')
-    })
+      return await takeAndCompareScreenshot('simple', 'narrow');
+    });
     it('/black', async function () {
-      return await takeAndCompareScreenshot('black', 'narrow')
-    })
+      return await takeAndCompareScreenshot('black', 'narrow');
+    });
     it('/image', async function () {
-      return await takeAndCompareScreenshot('image', 'narrow')
-    })
-  })
-})
+      return await takeAndCompareScreenshot('image', 'narrow');
+    });
+  });
+});
--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-import { Editor, Editable, Settings, ImageField } from 'haiku-light-editor'
-import 'haiku-light-editor/dist/index.css'
+import { Editor, Editable, Settings, ImageField } from 'haiku-light-editor';
+import 'haiku-light-editor/dist/index.css';
 
 const settings: Settings = {
   width: 300,
   height: 200
-}
+};
 
 const editable: Editable = {
   width: 20,
   height: 20
-}
+};
 
 const imageField: ImageField = {
   type: 'image',
@@ -30,7 +30,7 @@ const imageField: ImageField = {
   imageUrl: '/wolf.png',
   imageFlip: false,
   imageRotation: 0
-}
+};
 
 const App = () => {
   return (
@@ -54,10 +54,62 @@ const App = () => {
             editable={{ ...editable, fields: [imageField] }}
           />
         </Route>
+        <Route
+          path='/exifl:id'
+          render={({ match }) => {
+            return (
+              <Editor
+                settings={settings}
+                editable={{
+                  ...editable,
+                  fields: [
+                    {
+                      ...imageField,
+                      imageWidth: 1800,
+                      imageHeight: 1200,
+                      x: 1,
+                      y: 4,
+                      width: 18,
+                      height: 12,
+                      scale: 0.01,
+                      imageUrl: `/exif/Landscape_${match.params.id}.jpg`
+                    }
+                  ]
+                }}
+              />
+            );
+          }}
+        ></Route>
+        <Route
+          path='/exifp:id'
+          render={({ match }) => {
+            return (
+              <Editor
+                settings={settings}
+                editable={{
+                  ...editable,
+                  fields: [
+                    {
+                      ...imageField,
+                      imageWidth: 1200,
+                      imageHeight: 1800,
+                      x: 4,
+                      y: 1,
+                      width: 12,
+                      height: 18,
+                      scale: 0.01,
+                      imageUrl: `/exif/Portrait_${match.params.id}.jpg`
+                    }
+                  ]
+                }}
+              />
+            );
+          }}
+        ></Route>
         <Route path='/'></Route>
       </Switch>
     </Router>
-  )
-}
+  );
+};
 
-export default App
+export default App;
